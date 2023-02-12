1e4efb05bc4edb330d75f4c5b1cf223f98c03d77
Liam M <lmcrailt4@gmail.com>
2023-02-12 00:12:49 +0000

    Draw user interface (Game area, next piece, score).

--- SimpleTetris/Source.cpp
+++ SimpleTetris/Source.cpp
@@ -4,10 +4,17 @@
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 #include "Utilities.h"
-#define WINDOW_WIDTH 502
-#define WINDOW_HEIGHT 602
+
+constexpr auto WINDOW_WIDTH = 502;
+constexpr auto WINDOW_HEIGHT = 560;
+constexpr auto GAME_WIDTH = 10;
+constexpr auto GAME_HEIGHT = 20;
+constexpr auto TILE_SIZE = 25;
 
 SDL_Texture* backTexture;
+SDL_Rect* gameArea;
+SDL_Rect* nextPieceArea;
+SDL_Rect* scoreArea;
 
 bool init()
 {
@@ -56,6 +63,14 @@ void Render(SDL_Renderer* renderer, SDL_Rect* screen)
 {
 	// Draw background.
 	SDL_RenderCopy(renderer, backTexture, NULL, screen);
+
+	// Draw UI.
+	SDL_SetRenderDrawColor(renderer, 30, 30, 30, 100);
+
+	SDL_RenderFillRect(renderer, gameArea);
+	SDL_RenderFillRect(renderer, nextPieceArea);
+	SDL_RenderFillRect(renderer, scoreArea);
+
 	SDL_RenderPresent(renderer);
 }
 
@@ -71,9 +86,16 @@ int main(int argc, char* argv[])
 	// Create Window.
 	SDL_Window* window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	
 	SDL_Rect* screen = new SDL_Rect({ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT });
 	SDL_Colour baseColour{ 33, 33, 40, 255 };
 	backTexture = DrawGradient(renderer, &baseColour, WINDOW_WIDTH, WINDOW_HEIGHT, 0.6, 2);
+
+	// UI Specifications.
+	gameArea = new SDL_Rect({ 30, 30, TILE_SIZE * GAME_WIDTH, TILE_SIZE * GAME_HEIGHT });
+	nextPieceArea = new SDL_Rect({ 310, 30, 162, 162 });
+	scoreArea = new SDL_Rect({ 310, 222, 162, 50 });
 
 	bool isRunning = true;
 
